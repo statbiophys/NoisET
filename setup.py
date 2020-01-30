7cd89e866be55c8ddacdb8c02239df5b81801982
mbensouda <49962664+mbensouda@users.noreply.github.com>
2020-01-30 18:50:11 +0100

    Add files via upload

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh :
     long_description = fh.read
 
 setuptools.setup(
-    name='noisydiff',
+    name='noisettes',
     version= "0.0.1",
     author="Meriem Bensouda",
     author_email="meriem.bensoudakoraichi@gmail.com",
